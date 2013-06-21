00d090402209ac9d27c36e7f73b6ad1cbe5b58de
Jorge Flores <jorge@ievolutioned.com>
2013-06-21 18:52:36 -0500

    New application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anuncialo::Application.config.secret_token = 'd1e36c16843b1c31bedbddda16f78182d038a2fe49fdbce50d8712ec4a624f4b1800b6950924a6dcdaebfc9123ba610307592fa122f202ca8b56fc0f95546082'
